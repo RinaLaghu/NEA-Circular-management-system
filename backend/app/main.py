eba829c052dfbf01f66c98ad8260cc88ada6fff8
Subekshyeah <Kadelsubekshya@gmail.com>
2026-04-29 17:49:07 +0545

    routes renaming

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,7 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from app.models.circular import Circular
 from fastapi.staticfiles import StaticFiles
 from app.db.database import Base, engine
-from app.api.routes.department_login import router as dept_router
+from app.api.routes.departments import router as dept_router
 from app.api.routes.circular import router as circular_router
 from app.deps.auth import get_current_user
 
