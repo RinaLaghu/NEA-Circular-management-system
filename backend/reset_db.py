27af3cea2b0a7343868ce12022aa497893e81817
Binu Shrestha <binushrestha484@gmail.com>
2026-05-03 19:20:54 +0545

    updated reset_db.py by adding it for circulars

--- backend/reset_db.py
+++ backend/reset_db.py
@@ -2,7 +2,7 @@
 from app.db.database import engine, Base
 
 # Import all models so they are registered with Base
-from app.models import directorate, dept
+from app.models import directorate, dept, circular, user
 
 if __name__ == "__main__":
     print("Dropping all tables...")
